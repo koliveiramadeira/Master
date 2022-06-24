c1af14f64db0f16d3a1048d4543fd0315436f4cf
koliveiramadeira <karlaoliveira.madeira@gmail.com>
2022-06-23 22:27:22 -0300

    Mapeamento de elementos

--- src/test/java/Elementos/Elementos.java
+++ src/test/java/Elementos/Elementos.java
@@ -1,10 +1,23 @@
 package Elementos;
 
+import org.openqa.selenium.By;
+
 public class Elementos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+private By btnAbrirConta = By.cssSelector("body > div.viewport-primary > nav.nav-desktop > div.right > div.nav-item.nav-item-nova-conta.open > a");
+private By btnAbrirContaPf = By.id("bt1");
+private By campoNome = By.name("nome");
+private By campoTelefone = By.id("telefone");
+private By campoEmail = By.id("email");
+private By campoCpf = By.id("cpf");
+private By btnEnviar = By.id("btnEnviar");
+private By textoValidacao = By.cssSelector("#pu-modalSucesso > div > div:nth-child(2) > p > span:nth-child(1)");
+
+		
+		
+		
+		
 	}
 
-}
+
